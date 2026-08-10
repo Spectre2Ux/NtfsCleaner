06b0ad41e6f058ed61bced68f5b8c6f0aae9091f
Spectre2Ux <assembly2spectre@gmail.com>
2026-08-10 18:05:40 +0300

    Update build script for version 0.3

--- src/build.bat
+++ src/build.bat
@@ -1,2 +1,2 @@
 @echo off
-cl /EHsc /MT /Fe:NtfsCleaner_v0.1.exe NtfsCleaner_v0.1.c user32.lib kernel32.lib advapi32.lib gdi32.lib
+cl /EHsc /MT /Fe:NtfsCleaner_v0.3.exe NtfsCleaner_v0.3.c user32.lib kernel32.lib advapi32.lib gdi32.lib
